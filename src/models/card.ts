5dc1cb176a9a05b01699ab8a95269aab0e16f4bf
Kettrin62 <kettykazakova@yandex.ru>
2022-09-21 10:09:01 +0300

    feat: models

---
+++ src/models/card.ts
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from "mongoose";
+
+interface ICard {
+  name: string;
+  link: string;
+  owner: Schema.Types.ObjectId;
+  likes: Array<Schema.Types.ObjectId>;
+  createdAt: Date;
+}
+
+const cardSchema = new mongoose.Schema<ICard>({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    required: true
+  },
+  likes: [{
+    type: Schema.Types.ObjectId,
+    default: []
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+// напишите код здесь
+export default mongoose.model('card', cardSchema);
