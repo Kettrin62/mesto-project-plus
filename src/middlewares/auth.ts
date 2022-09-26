e6ccae82763e374cf9a44da3dac3beec5fbe2464
Kettrin62 <kettykazakova@yandex.ru>
2022-09-26 15:55:03 +0300

    feat: middlewares/auth.ts

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,45 @@
+import {
+  Request,
+  Response,
+  NextFunction,
+} from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+interface SessionRequest extends Request {
+  user?: string | JwtPayload;
+}
+
+const handleAuthError = (res: Response) => {
+  res
+    .status(401)
+    .send({ message: 'Необходима авторизация' });
+};
+
+const extractBearerToken = (
+  token: string,
+) => token.replace('Bearer ', '');
+
+export default (
+  req: SessionRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return handleAuthError(res);
+  }
+
+  const token = extractBearerToken(authorization);
+  let payload;
+
+  try {
+    payload = jwt.verify(token, 'some-secret-key');
+  } catch (err) {
+    return handleAuthError(res);
+  }
+
+  req.user = payload;
+
+  next();
+};
